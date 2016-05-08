10577f45ec6df4ac1db621ed0808ef676b94e6e0
abhishektyagi <abhishek.tyagi.16@gmail.com>
2016-05-08 21:54:49 +0530

    completed the first part of the task

---
+++ src/ImageRequest.java
@@ -0,0 +1,55 @@
+
+public class ImageRequest {
+	private int numOfRequest, responseTime, totalTime;
+	private String request;
+	public ImageRequest(String request, int responseTime, int totalTime) {
+		this.request = request;
+		this.responseTime = responseTime;
+		this.totalTime = totalTime;
+		numOfRequest = 1;
+	}
+	
+	public String getRequest() {
+		return request;
+	}
+	
+	public int getNumOfRequests() {
+		return numOfRequest;
+	}
+	
+	public void incrementRequests() {
+		numOfRequest++;
+	}
+	
+	public void setNumOfRequests(int num) {
+		this.numOfRequest = num;
+	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof ImageRequest) {
+			return ((ImageRequest)obj).getRequest().equals(getRequest());
+		}
+		else return false;
+	}
+	
+	public void addResponseTime(int time) {
+		responseTime += time;
+	}
+	
+	public void addTotalTime(int time) {
+		totalTime += time;
+	}
+	
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		String str = "Path : " + request + "\n Number of requests : " + numOfRequest + "\n Average response time : " + responseTime/numOfRequest
+				+ "Average total time : " + totalTime/numOfRequest;
+		return str;
+	}
+
+	public int hashCode(){
+		return request.hashCode();
+    }
+}
--- src/ParseLogFile.java
+++ src/ParseLogFile.java
@@ -1,8 +1,17 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ParseLogFile {
 	public static void main(String[] args) {
@@ -10,29 +19,90 @@ public class ParseLogFile {
 		System.out.println("Enter folder path");
 		
 		try {
-			String path = br.readLine();
+			String path = "/Users/abhishektyagi/Documents/untitledfolder";//br.readLine();
 			System.out.println("Enter time range separated by space(Ex: 14141414 15151515515)");
 			String[] time = br.readLine().split("//s+");
 			System.out.println("Enter the number of items to include in output");
 			int n = Integer.parseInt(br.readLine());
-			String option = args[0];
+			String option = "1";//args[0];
+			
+			
 			
 			if(option.equals("1")) {
-				PriorityQueue<Image> maxHeap = new PriorityQueue<>(new Comparator<Image>() {
+				HashMap<String, ImageRequest> map = new HashMap<>();
+				PriorityQueue<ImageRequest> heap = new PriorityQueue<>(n, new Comparator<ImageRequest>() {
 
 					@Override
-					public int compare(Image o1, Image o2) {
+					public int compare(ImageRequest o1, ImageRequest o2) {
 						// TODO Auto-generated method stub
-						return o2.getNumOfRequests()-o1.getNumOfRequests();
+						return o1.getNumOfRequests()-o2.getNumOfRequests();
 					}
-					
 				});
+				File folder = new File(path);
+				for (File fileEntry : folder.listFiles()) {
+					if (fileEntry.isFile()) {
+						BufferedReader fileReader = new BufferedReader(new FileReader(fileEntry));
+						String line = fileReader.readLine();
+						while(line != null) {
+//							System.out.println(line);
+							String regex = "timestamp";
+							int index = line.indexOf(regex);
+							if(index != -1) {
+								String jsonString = line.substring(index-2);
+								JSONObject json = new JSONObject(jsonString);
+								int timestamp = json.getInt("timestamp");
+								String requestor = json.getString("remote_addr");
+								int responseTime = json.getInt("response_time");
+								int totalTime = json.getInt("total_time");
+								String request = json.getString("http_request");
+								String[] requestParams = request.split("\\s+");
+								String imagePath = requestParams[1];
+								if(imagePath.substring(imagePath.length()-5).equals(".jpeg")) {
+									ImageRequest imageRequest = new ImageRequest(imagePath, responseTime, totalTime);
+									if(map.containsKey(imagePath)) {
+										ImageRequest temp = map.get(imagePath);
+										temp.incrementRequests();
+										temp.addResponseTime(responseTime);
+										temp.addTotalTime(totalTime);
+										
+										ImageRequest head = heap.peek();
+										if(head.getNumOfRequests() < temp.getNumOfRequests()) {
+											heap.remove(imageRequest);			//If heap doesn't contain the element then remove() method will return false but its doesn't matter
+											imageRequest.setNumOfRequests(temp.getNumOfRequests());
+											heap.add(imageRequest);
+											
+										}
+									}
+									else {
+										
+										map.put(imagePath, imageRequest);
+										
+										if(heap.isEmpty() || heap.size() < n) heap.add(imageRequest);
+									}
+								}
+//								System.out.println(jsonString);
+							}
+//							System.out.println(index);
+							line = fileReader.readLine();
+							
+							
+						}
+				    }
+			    }
+				
+				System.out.println("Top " + n + " image request details are: ");
+				while(!heap.isEmpty()) {
+					ImageRequest head = heap.poll();
+					System.out.println(head.toString());	//Top N will be printed from least to highest
+				}
+				
+				
 				
 			}
 			else {
 				
 			}
-		} catch (IOException e) {
+		} catch (IOException | JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
