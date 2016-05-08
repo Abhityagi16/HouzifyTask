3b014ba6cf1efd74b8ece3f1cfd73a35046e04d8
abhishektyagi <abhishek.tyagi.16@gmail.com>
2016-05-08 19:57:30 +0530

    override equal and hascode of Image class

--- src/Image.java
+++ src/Image.java
@@ -18,4 +18,15 @@ public class Image {
 	public void incrementRequests() {
 		numOfRequest++;
 	}
+	
+	public boolean equals(Object obj) {
+		if(obj instanceof Image) {
+			return ((Image)obj).getRequest().equals(getRequest());
+		}
+		else return false;
+	}
+	
+	public int hashCode(){
+		return request.hashCode();
+    }
 }
